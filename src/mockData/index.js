24aeeb914d8bb3e0e77a7dd643d7502aea4611a4
tanveerns <tanveeralam229@gmail.com>
2022-07-16 00:32:33 +0530

    initial

---
+++ src/mockData/index.js
@@ -0,0 +1,18 @@
+export const mainNav = [
+    {
+        display: "Trang chủ",
+        path: "/"
+    },
+    {
+        display: "Sản phẩm",
+        path: "/catalog"
+    },
+    {
+        display: "Phụ kiện",
+        path: "/accessories"
+    },
+    {
+        display: "Liên hệ",
+        path: "/contact"
+    }
+]
